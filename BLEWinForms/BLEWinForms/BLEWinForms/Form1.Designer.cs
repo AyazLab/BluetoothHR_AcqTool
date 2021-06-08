ceb2d0e34f518cf4b62908763e0108b2ca8386ef
Nick DeFilippis <ndefilippis98@gmail.com>
2021-06-08 17:03:58 -0400

    Add winforms for device enumeration

---
+++ BLEWinForms/BLEWinForms/BLEWinForms/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace BLEWinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.scanButton = new System.Windows.Forms.Button();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.deviceListView = new System.Windows.Forms.DataGridView();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.deviceListView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 23.61702F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 76.38298F));
+            this.tableLayoutPanel1.Controls.Add(this.scanButton, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.connectButton, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.deviceListView, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.statusStrip, 0, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 434F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(940, 599);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // scanButton
+            // 
+            this.scanButton.Location = new System.Drawing.Point(3, 3);
+            this.scanButton.Name = "scanButton";
+            this.scanButton.Size = new System.Drawing.Size(188, 58);
+            this.scanButton.TabIndex = 1;
+            this.scanButton.Text = "Start Scan";
+            this.scanButton.UseVisualStyleBackColor = true;
+            this.scanButton.Click += new System.EventHandler(this.scanButton_Click);
+            // 
+            // connectButton
+            // 
+            this.connectButton.Enabled = false;
+            this.connectButton.Location = new System.Drawing.Point(224, 3);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(188, 58);
+            this.connectButton.TabIndex = 0;
+            this.connectButton.Text = "Connect";
+            this.connectButton.UseVisualStyleBackColor = true;
+            // 
+            // deviceListView
+            // 
+            this.deviceListView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.deviceListView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.deviceListView.Location = new System.Drawing.Point(224, 148);
+            this.deviceListView.Name = "deviceListView";
+            this.deviceListView.RowHeadersWidth = 102;
+            this.deviceListView.RowTemplate.Height = 49;
+            this.deviceListView.Size = new System.Drawing.Size(713, 428);
+            this.deviceListView.TabIndex = 2;
+            this.deviceListView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.deviceListView_CellContentClick);
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.ImageScalingSize = new System.Drawing.Size(40, 40);
+            this.statusStrip.Location = new System.Drawing.Point(0, 579);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(221, 20);
+            this.statusStrip.TabIndex = 3;
+            this.statusStrip.Text = "statusStrip1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 41F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(940, 599);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.deviceListView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.Button scanButton;
+        private System.Windows.Forms.DataGridView deviceListView;
+        private System.Windows.Forms.StatusStrip statusStrip;
+    }
+}
---
+++ BLEWinForms/BLEWinForms/BLEWinForms/Form1.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using Windows.Devices.Bluetooth;
+using Windows.Devices.Enumeration;
+using Windows.UI.Core;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using System.Windows.Forms;
+using SDKTemplate;
+
+namespace BLEWinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            deviceListView.DataSource = KnownDevices;
+            //deviceListView.AutoGenerateColumns = true;
+        }
+
+        private BindingList<BluetoothLEDeviceDisplay> KnownDevices = new BindingList<BluetoothLEDeviceDisplay>();
+        private List<DeviceInformation> UnknownDevices = new List<DeviceInformation>();
+        private BluetoothLEDeviceDisplay selectedDevice;
+
+        private DeviceWatcher deviceWatcher;
+
+        #region UI Code
+
+        private bool IsNotNull(object value) => (value != null);
+        #endregion
+
+        #region Device discovery
+
+        /// <summary>
+        /// Starts a device watcher that looks for all nearby Bluetooth devices (paired or unpaired). 
+        /// Attaches event handlers to populate the device collection.
+        /// </summary>
+        private void StartBleDeviceWatcher()
+        {
+            // Additional properties we would like about the device.
+            // Property strings are documented here https://msdn.microsoft.com/en-us/library/windows/desktop/ff521659(v=vs.85).aspx
+            string[] requestedProperties = { "System.Devices.Aep.DeviceAddress", "System.Devices.Aep.IsConnected", "System.Devices.Aep.Bluetooth.Le.IsConnectable" };
+
+            // BT_Code: Example showing paired and non-paired in a single query.
+            string aqsAllBluetoothLEDevices = "(System.Devices.Aep.ProtocolId:=\"{bb7bb05e-5972-42b5-94fc-76eaa7084d49}\")";
+
+            deviceWatcher =
+                    DeviceInformation.CreateWatcher(
+                        aqsAllBluetoothLEDevices,
+                        requestedProperties,
+                        DeviceInformationKind.AssociationEndpoint);
+
+            // Register event handlers before starting the watcher.
+            deviceWatcher.Added += DeviceWatcher_Added;
+            deviceWatcher.Updated += DeviceWatcher_Updated;
+            deviceWatcher.Removed += DeviceWatcher_Removed;
+            deviceWatcher.EnumerationCompleted += DeviceWatcher_EnumerationCompleted;
+            deviceWatcher.Stopped += DeviceWatcher_Stopped;
+
+            // Start over with an empty collection.
+            KnownDevices.Clear();
+
+            // Start the watcher. Active enumeration is limited to approximately 30 seconds.
+            // This limits power usage and reduces interference with other Bluetooth activities.
+            // To monitor for the presence of Bluetooth LE devices for an extended period,
+            // use the BluetoothLEAdvertisementWatcher runtime class. See the BluetoothAdvertisement
+            // sample for an example.
+            deviceWatcher.Start();
+        }
+
+        /// <summary>
+        /// Stops watching for all nearby Bluetooth devices.
+        /// </summary>
+        private void StopBleDeviceWatcher()
+        {
+            if (deviceWatcher != null)
+            {
+                // Unregister the event handlers.
+                deviceWatcher.Added -= DeviceWatcher_Added;
+                deviceWatcher.Updated -= DeviceWatcher_Updated;
+                deviceWatcher.Removed -= DeviceWatcher_Removed;
+                deviceWatcher.EnumerationCompleted -= DeviceWatcher_EnumerationCompleted;
+                deviceWatcher.Stopped -= DeviceWatcher_Stopped;
+
+                // Stop the watcher.
+                deviceWatcher.Stop();
+                deviceWatcher = null;
+            }
+        }
+
+        private BluetoothLEDeviceDisplay FindBluetoothLEDeviceDisplay(string id)
+        {
+            foreach (BluetoothLEDeviceDisplay bleDeviceDisplay in KnownDevices)
+            {
+                if (bleDeviceDisplay.Id == id)
+                {
+                    return bleDeviceDisplay;
+                }
+            }
+            return null;
+        }
+
+        private DeviceInformation FindUnknownDevices(string id)
+        {
+            foreach (DeviceInformation bleDeviceInfo in UnknownDevices)
+            {
+                if (bleDeviceInfo.Id == id)
+                {
+                    return bleDeviceInfo;
+                }
+            }
+            return null;
+        }
+
+        private void DeviceWatcher_Added(DeviceWatcher sender, DeviceInformation deviceInfo)
+        {
+            // We must update the collection on the UI thread because the collection is databound to a UI element.
+            BeginInvoke((Action)(() =>
+            {
+                lock (this)
+                {
+                    Debug.WriteLine(String.Format("Added {0}{1}", deviceInfo.Id, deviceInfo.Name));
+
+                    // Protect against race condition if the task runs after the app stopped the deviceWatcher.
+                    if (sender == deviceWatcher)
+                    {
+                        // Make sure device isn't already present in the list.
+                        if (FindBluetoothLEDeviceDisplay(deviceInfo.Id) == null)
+                        {
+                            if (deviceInfo.Name != string.Empty)
+                            {
+                                // If device has a friendly name display it immediately.
+                                KnownDevices.Add(new BluetoothLEDeviceDisplay(deviceInfo));
+                                deviceListView.Refresh();
+                            }
+                            else
+                            {
+                                // Add it to a list in case the name gets updated later. 
+                                UnknownDevices.Add(deviceInfo);
+                            }
+                        }
+
+                    }
+                }
+            }));
+        }
+
+        private void DeviceWatcher_Updated(DeviceWatcher sender, DeviceInformationUpdate deviceInfoUpdate)
+        {
+            // We must update the collection on the UI thread because the collection is databound to a UI element.
+            BeginInvoke((Action)(() =>
+            {
+                lock (this)
+                {
+                    Debug.WriteLine(String.Format("Updated {0}{1}", deviceInfoUpdate.Id, ""));
+
+                    // Protect against race condition if the task runs after the app stopped the deviceWatcher.
+                    if (sender == deviceWatcher)
+                    {
+                        BluetoothLEDeviceDisplay bleDeviceDisplay = FindBluetoothLEDeviceDisplay(deviceInfoUpdate.Id);
+                        if (bleDeviceDisplay != null)
+                        {
+                            // Device is already being displayed - update UX.
+                            bleDeviceDisplay.Update(deviceInfoUpdate);
+                            return;
+                        }
+
+                        DeviceInformation deviceInfo = FindUnknownDevices(deviceInfoUpdate.Id);
+                        if (deviceInfo != null)
+                        {
+                            deviceInfo.Update(deviceInfoUpdate);
+                            // If device has been updated with a friendly name it's no longer unknown.
+                            if (deviceInfo.Name != String.Empty)
+                            {
+                                KnownDevices.Add(new BluetoothLEDeviceDisplay(deviceInfo)) ;
+                                deviceListView.Refresh();
+                                UnknownDevices.Remove(deviceInfo);
+                            }
+                        }
+                    }
+                }
+            }));
+        }
+
+        private void DeviceWatcher_Removed(DeviceWatcher sender, DeviceInformationUpdate deviceInfoUpdate)
+        {
+            // We must update the collection on the UI thread because the collection is databound to a UI element.
+            BeginInvoke((Action)(() =>
+            {
+                lock (this)
+                {
+                    Debug.WriteLine(String.Format("Removed {0}{1}", deviceInfoUpdate.Id, ""));
+
+                    // Protect against race condition if the task runs after the app stopped the deviceWatcher.
+                    if (sender == deviceWatcher)
+                    {
+                        // Find the corresponding DeviceInformation in the collection and remove it.
+                        BluetoothLEDeviceDisplay bleDeviceDisplay = FindBluetoothLEDeviceDisplay(deviceInfoUpdate.Id);
+                        if (bleDeviceDisplay != null)
+                        {
+                            KnownDevices.Remove(bleDeviceDisplay);
+                            deviceListView.Refresh();
+                        }
+
+                        DeviceInformation deviceInfo = FindUnknownDevices(deviceInfoUpdate.Id);
+                        if (deviceInfo != null)
+                        {
+                            UnknownDevices.Remove(deviceInfo);
+                        }
+                    }
+                }
+            }));
+        }
+
+        private void DeviceWatcher_EnumerationCompleted(DeviceWatcher sender, object e)
+        {
+            // We must update the collection on the UI thread because the collection is databound to a UI element.
+            BeginInvoke((Action)(() =>
+            {
+                // Protect against race condition if the task runs after the app stopped the deviceWatcher.
+                if (sender == deviceWatcher)
+                {
+                    statusStrip.Text = $"{KnownDevices.Count} devices found. Enumeration completed.";
+                }
+            }));
+        }
+
+        private void DeviceWatcher_Stopped(DeviceWatcher sender, object e)
+        {
+            // We must update the collection on the UI thread because the collection is databound to a UI element.
+            BeginInvoke((Action)(() =>
+            {
+                // Protect against race condition if the task runs after the app stopped the deviceWatcher.
+                if (sender == deviceWatcher)
+                {
+                    statusStrip.Text = "No longer watching for devices.";
+                }
+            }));
+        }
+        #endregion
+
+        #region Pairing
+
+        private void ConnectButton_Click()
+        {
+            //TODO: this
+        }
+
+        #endregion
+
+        private void scanButton_Click(object sender, EventArgs e)
+        {
+            if (deviceWatcher == null)
+            {
+                StartBleDeviceWatcher();
+                scanButton.Text = "Stop scan";
+                statusStrip.Text = "Device watcher started.";
+            }
+            else
+            {
+                StopBleDeviceWatcher();
+                scanButton.Text = "Start scan";
+                statusStrip.Text = "Device watcher stopped.";
+            }
+        }
+
+        private void deviceListView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (0 <= e.RowIndex && e.RowIndex < KnownDevices.Count)
+            {
+                selectedDevice = KnownDevices[e.RowIndex];
+                connectButton.Enabled = true;
+            }
+            else {
+                selectedDevice = null;
+                connectButton.Enabled = false;
+            }
+            
+        }
+    }
+}
